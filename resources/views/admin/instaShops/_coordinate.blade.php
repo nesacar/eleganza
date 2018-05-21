1e6bac89c581432b87d26e453030eb61be25bb77
nesacar <nebojsart1409@yahoo.com>
2018-05-21 08:38:55 +0200

    blate remplate refactoring

--- resources/views/admin/instaShops/_coordinate.blade.php
+++ resources/views/admin/instaShops/_coordinate.blade.php
@@ -1,34 +1,17 @@
-@if(!empty($coordinate))
-    <div style="padding: 10px 0">
-        <div class="form-group">
-            <label for="publish" class="col-sm-2 control-label">Pin {{ $key }}</label>
-            <div class="col-sm-8">
-                {!! Form::select('products[]', $productIds, $coordinate->product_id, ['class' => 'form-control products']) !!}
-                {!! Form::hidden('x[]', $coordinate->x) !!}
-                {!! Form::hidden('y[]', $coordinate->y) !!}
-            </div>
-            <div class="col-md-2">
-                <span class="fa fa-remove" style="padding: 11px 0; cursor: pointer;"></span>
-            </div>
+<div style="padding: 10px 0">
+    <div class="form-group">
+        <label for="publish" class="col-sm-2 control-label">Pin {{ $pin }}</label>
+        <div class="col-sm-8">
+            {!! Form::select('products[]', $productIds, null, ['class' => 'form-control products']) !!}
+            {!! Form::hidden('x[]', $x) !!}
+            {!! Form::hidden('y[]', $y) !!}
         </div>
-        <hr>
-    </div>
-@else
-    <div style="padding: 10px 0">
-        <div class="form-group">
-            <label for="publish" class="col-sm-2 control-label">Pin {{ $pin }}</label>
-            <div class="col-sm-8">
-                {!! Form::select('products[]', $productIds, null, ['class' => 'form-control products']) !!}
-                {!! Form::hidden('x[]', $x) !!}
-                {!! Form::hidden('y[]', $y) !!}
-            </div>
-            <div class="col-md-2">
-                <span class="fa fa-remove" style="padding: 11px 0; cursor: pointer;"></span>
-            </div>
+        <div class="col-md-2">
+            <span class="fa fa-remove" style="padding: 11px 0; cursor: pointer;"></span>
         </div>
-        <hr>
     </div>
-@endif
+    <hr>
+</div>
 
 <script>
     $('.fa-remove').click(function(){
--- resources/views/admin/instaShops/edit.blade.php
+++ resources/views/admin/instaShops/edit.blade.php
@@ -86,7 +86,20 @@
                 <div class="panel-body" id="place">
                     @if(count($instaShop->coordinate)>0)
                         @foreach($instaShop->coordinate as $key => $coordinate)
-                            @include('admin.instaShops._coordinate', ['coordinate' => $coordinate, 'key' => ++$key])
+                            <div style="padding: 10px 0">
+                                <div class="form-group">
+                                    <label for="publish" class="col-sm-2 control-label">Pin {{ $key }}</label>
+                                    <div class="col-sm-8">
+                                        {!! Form::select('products[]', $productIds, $coordinate->product_id, ['class' => 'form-control products']) !!}
+                                        {!! Form::hidden('x[]', $coordinate->x) !!}
+                                        {!! Form::hidden('y[]', $coordinate->y) !!}
+                                    </div>
+                                    <div class="col-md-2">
+                                        <span class="fa fa-remove" style="padding: 11px 0; cursor: pointer;"></span>
+                                    </div>
+                                </div>
+                                <hr>
+                            </div>
                         @endforeach
                     @endif
                 </div>
