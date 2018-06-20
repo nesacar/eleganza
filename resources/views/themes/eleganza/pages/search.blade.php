1510192d06159890eab79c70495c159da68a1912
VacaSan <kotaracvladan@gmail.com>
2018-06-20 10:37:44 +0200

    Extracted product to component.

--- resources/views/themes/eleganza/pages/search.blade.php
+++ resources/views/themes/eleganza/pages/search.blade.php
@@ -64,32 +64,8 @@
             @endif
             <ul class="product-list">
                 @foreach($products as $product)
-                    @php $link = url(\App\Product::getProductLink($product->id)); @endphp
-                    <li class="product-item product-list__item product-list__item--search with-shadow">
-                        <a href="{{ $link }}">
-                            <div class=product-item__img-box>
-                                {!! HTML::Image($product->image, $product->title) !!}
-                                <ul class=product-item__actions>
-                                    <li class="icon-btn icon-btn--inverse"><a href="{{ url('add-to-cart/'.$product->id) }}" class="addCart" style="z-index: 1;"><i class="fas fa-shopping-cart"></i></a> </li>
-                                    <li class="icon-btn icon-btn--inverse"> <a href="{{ url('add-to-wishlist/'.$product->id) }}" class="addWish" style="z-index: 1;"><i class="fas fa-heart"></i></a> </li>
-                                    <li class="icon-btn icon-btn--inverse"> <a href="{{ $link }}" style="z-index: 1;"><i class="fas fa-search"></i></a> </li>
-                                </ul>
-                            </div>
-                            <div class=product-item__info-box>
-                                <span class=product-item__brand>@if(isset($product->brand)) {{ $product->brand->title }} @endif</span>
-                                <h2 class=product-item__name>{{ $product->title }}</h2>
-                                @if($product->discount != null && $product->discount > 0)
-                                    <span class=product-item__price>{{ $product->price_outlet }}</span>
-                                @else
-                                    <span class=product-item__price>{{ $product->price_small }}</span>
-                                @endif
-                            </div>
-                            <button class="e-btn e-btn--primary e-btn--block">saznaj više</button>
-                        </a>
-                        @if($product->discount != null && $product->discount > 0)
-                            <div class="status status--sale">popust {{ $product->discount }}</div>
-                        @endif
-                    </li>
+                  @component('themes.'.$theme->slug.'.components.product', ['product' => $product])
+                  @endcomponent
                 @endforeach
             </ul>
         </div>
--- resources/views/themes/eleganza/pages/shop-category.blade.php
+++ resources/views/themes/eleganza/pages/shop-category.blade.php
@@ -76,33 +76,8 @@
             </div>
             <ul class="product-list">
                 @foreach($data['products'] as $product)
-                    <li class="product-item product-list__item with-shadow">
-                        <a href="{{ $product->getLink() }}">
-                            <div class=product-item__img-box>
-                                {!! HTML::Image($product->image, $product->title) !!}
-                                <ul class=product-item__actions>
-                                    <li class="icon-btn icon-btn--inverse"><a href="{{ url('add-to-cart/'.$product->id) }}" class="addCart" style="z-index: 1;"><i class="fas fa-shopping-cart"></i></a> </li>
-                                    <li class="icon-btn icon-btn--inverse"> <a href="{{ url('add-to-wishlist/'.$product->id) }}" class="addWish" style="z-index: 1;"><i class="fas fa-heart"></i></a> </li>
-                                    <li class="icon-btn icon-btn--inverse"> <a href="{{ $product->getLink() }}" style="z-index: 1;"><i class="fas fa-search"></i></a> </li>
-                                </ul>
-                            </div>
-                            <div class=product-item__info-box>
-                                <span class=product-item__brand>@if(isset($product->brand)) {{ $product->brand->title }} @endif</span>
-                                <h2 class=product-item__name>{{ $product->title }}</h2>
-                                @if($product->discount != null && $product->discount > 0)
-                                    <span class=product-item__price>{{ $product->price_outlet }}</span>
-                                @else
-                                    <span class=product-item__price>{{ $product->price_small }}</span>
-                                @endif
-                            </div>
-                            <button class="e-btn e-btn--primary e-btn--block">saznaj više</button>
-                        </a>
-                        @if($product->discount != null && $product->discount > 0)
-                            <div class="status status--sale">popust {{ $product->discount }}</div>
-                        @elseif(\App\Product::isNewProduct($product))
-                            <div class="status status--sale">novo</div>
-                        @endif
-                    </li>
+                  @component('themes.'.$theme->slug.'.components.product', ['product' => $product])
+                  @endcomponent
                 @endforeach
             </ul>
         </div>
